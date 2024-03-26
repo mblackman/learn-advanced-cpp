cd98da09a0275188475367358f8db0991000308b
Matt Blackman <matt.blackman@pocketgems.com>
2024-03-25 18:51:43 -0600

    Parsing a text file example

---
+++ Files/reading-formatted-text.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+int main() {
+
+    string inputFileName = "stats.txt";
+    ifstream inFile;
+
+    inFile.open(inputFileName, ios::in);
+
+    if (!inFile.is_open()) {
+        return -1;
+    }
+    
+    string line;    
+    int population;
+
+    while (inFile) {
+        getline(inFile, line, ':');
+        inFile >> population;
+        inFile >> ws; // ws is a command to dispose the output from the read command.
+
+        if (!inFile) {
+            break;
+        }
+
+        cout << line << " --- " << population << endl;
+    }
+    
+    inFile.close();
+
+    return 0;
+}
